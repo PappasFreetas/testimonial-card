8c53f89c13d01b9db240804aa17027ee59855610
PappasFreetas <nick.pappas5424@gmail.com>
2025-05-05 21:48:40 -0400

    fix image

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
       <div className="h-[768px] w-[1440px] flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] py-[200px]">
-        <TestimonialCard name='Sarah Dole' handle='@sarahdole' imageUrl='../public/profile-thumbnail.png' post="I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"  />
+        <TestimonialCard name='Sarah Dole' handle='@sarahdole' imageUrl='/../public/profile-thumbnail.png' post="I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"  />
       </div>
   )
 }
